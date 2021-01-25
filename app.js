6af608c17bab6aefbe1822b043d0d47db904b051
neil-marcellini <neil.marcellini@gmail.com>
2021-01-25 10:22:34 -0800

    calling backend and showing a download link

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 // 1/15/21
 
 var file_uploader = document.getElementById('file_uploader')
+let backend_url = "http://localhost:8000/"
 
 const onFileChange = () => {
     var file_list = document.getElementById('file-list')
@@ -26,3 +27,39 @@ const onFileChange = () => {
     update_button.style.display = "block"
     console.log(files)
 }
+
+const onUpdate = () => {
+    // send files to backend for updating
+    const formData = new FormData();
+    let files = file_uploader.files
+    formData.append("files", files)
+    const percent_change = document.getElementById('percentage-change')
+    formData.append("percent_change", percent_change.value)
+    let round_fifty = document.getElementById('round-fifty')
+    formData.append("round_fifty", round_fifty.checked)
+    let round_direction = document.getElementById('round-direction')
+    formData.append("round_direction", round_direction.value)
+    var postFormOptions = {
+        method: "POST",
+        body: null
+    }
+    postFormOptions.body = formData
+    fetch(backend_url + 'files/', postFormOptions)
+        .then((response) => {
+            return response.arrayBuffer()
+        })
+        .then(data => {
+            console.log("update success")
+            const blob = new Blob([data], {
+              type: 'application/zip'
+            })
+            var url = URL.createObjectURL(blob)
+            const link = document.createElement('a');
+            link.href = url;
+            link.innerText = 'Download Updated Files';
+            document.body.appendChild(link);
+        })
+
+
+}
+
